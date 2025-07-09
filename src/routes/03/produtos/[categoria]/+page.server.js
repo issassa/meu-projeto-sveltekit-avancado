4647edaae464fbe2dd6002e295b2b40fff254582
issassa <isabella.santos2@estudante.ifms.edu.br>
2025-07-09 12:25:18 -0400

    resolução de frame

--- src/routes/03/produtos/[categoria]/+page.server.js
+++ src/routes/03/produtos/[categoria]/+page.server.js
@@ -1,19 +1,21 @@
-// src/routes/03/produtos/[categoria]/+page.server.js
+import { produtos } from '$lib/server/03/produtos.js';
 
-import { produtos as todosOsProdutos } from '$lib/server/03/produtos';
+export function load({ params, url }) {
+    let busca = url.searchParams.get('busca');
+    if (!busca) busca = '';
+    else busca = busca.toLowerCase();
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {
-    const categoriaParam = params.categoria.toLowerCase();
-
-    // FILTRAGEM CORRIGIDA AQUI:
-    // Verifica se o array 'categorias' do produto inclui a categoria que estamos buscando.
-    let produtosFiltrados = todosOsProdutos.filter(p =>
-        p.categorias && p.categorias.map(cat => cat.toLowerCase()).includes(categoriaParam)
-    );
-
-    return {
-        produtos: produtosFiltrados,
-        categoria: params.categoria
-    };
+    let produtosFiltrados = [];
+    for (const produto of produtos) {
+        if (
+            produto.categorias.includes(params.categoria) &&
+    (
+        busca == '' ||
+        produto.titulo.toLowerCase().includes(busca) ||
+        produto.descricao.toLowerCase().includes(busca)
+    )
+)
+            produtosFiltrados.push(produto);
+    }
+    return { produtosFiltrados, categoria: params.catgoria };
 }
